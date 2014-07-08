0beb2a69a066a23baed23f54c9862dc2376a1353
enfn2001@gmail.com <enfn2001@gmail.com>
2014-07-08 20:24:08 +0900

    로그인 폼
    로그인폼

--- views/loginView.php
+++ views/loginView.php
@@ -1,4 +1,13 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
+   // defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <p>login form</p>
+<div id="contents">
+	<div id="contents">
+		<div class="login">
+			<input class="id" type="text"></input>
+
+			<input class="pw" type="text"></input>
+		</div>
+	</div>
+</div>
